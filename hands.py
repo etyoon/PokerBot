329e634b4605067e3aacee8f9cdf2b48753f6d9b
etyoon <etyoon06@gmail.com>
2022-04-02 23:15:31 -0500

    changed functions to methods in order to hold onto suit and num dict

--- hands.py
+++ hands.py
@@ -1,27 +1,31 @@
 import random
 
-deck = [i for i in range(52)]
+class Poker:
 
-random.seed(5)
+    deck = [i for i in range(52)]
 
-random.shuffle(deck)
+    random.seed(5)
 
+    random.shuffle(deck)
 
-def suits_and_numbers(hand):
-    '''
-    given a five card hand determines number of cards and suits
-    '''
-    num = {}
-    suit = {}
-    for card in hand:
-        num[card % 13]=num.get(card % 13, 0) + 1
-        suit[card // 13]=suit.get(card // 13, 0) + 1
-    return(num, suit)
+    def __init__(self, hand):
+        self.hand = hand
+        self.suit = {}
+        self.num = {}
 
+    def suits_and_numbers(self):
+        '''
+        given a five card hand determines number of cards and suits
+        '''
 
+        for card in self.hand:
+            self.num[card % 13]=self.num.get(card % 13, 0) + 1
+            self.suit[card // 13]=self.suit.get(card // 13, 0) + 1
 
-def hands(hand):
-    '''
-    given a five card hand, determine what you have
-    '''
-    
+    def what_hand(self):
+        for nums in self.num:
+            if 
+
+test = Poker([5, 5+ 13, 2])
+test.suits_and_numbers()
+print(test.num)
