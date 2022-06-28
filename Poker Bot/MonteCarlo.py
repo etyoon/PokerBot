a82ad15c8728db3986dcdd8194ce6d01820cfaad
emy1563 <73967655+emy1563@users.noreply.github.com>
2022-06-28 14:57:15 -0500

    no change

--- Poker Bot/MonteCarlo.py
+++ Poker Bot/MonteCarlo.py
@@ -1,6 +1,15 @@
-import random
+from random import choice
 from cards import Dealer
+from hands import Poker
 
-simulation = Dealer()
-for i in range(3):
-    if simulation.deal = False
+class MonteCarlo:
+
+    def __init__(self, hand):
+        self.hand = hand
+
+    def deal_simulation():
+        for cards in range(10):
+            lst = []
+            lst.append(self.hand)
+            lst.append(choice[i for i in range(52) if i not in self.hand]
+        return()
